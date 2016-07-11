c687db91997bb2e461e597b75b225a68a7fb3cb0
Lars Elgaard Mikkelsen <lmk@trifork.com>
2016-07-11 10:05:27 +0200

    Fixed error in comments (iOS)

--- src/examples/MobilePayFruitshop-iPhone/MobilePayFruitshop/Externals/MobilePayManager.h
+++ src/examples/MobilePayFruitshop-iPhone/MobilePayFruitshop/Externals/MobilePayManager.h
@@ -327,7 +327,7 @@ DEPRECATED_MSG_ATTRIBUTE("Use setupWithMerchantId:(NSString * __nonnull)merchant
  *Max 255 characters and it must be an HTTPS:// endpoint
  *
  *Example:
- *[[MobilePayManager sharedInstance] setServerCallbackUrl:@"https://example.com/appswitchstatus?TimeStamp=2015-08-27T15:36:42.939&OrderId=34666357&MerchantId=APPDK1175851001&TransactionId=1234567890&Amount=00001500&Currency=DKK&Country=DK&PaymentStatus=RES&ReturnCode=00&ReasonCode=00"];
+ *[[MobilePayManager sharedInstance] setServerCallbackUrl:@"https://example.com/appswitchstatus"];
  *
  */
 
